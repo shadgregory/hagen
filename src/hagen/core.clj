96eb86d8695af57e3fa2fff0235cdbdb31a39149
Shad Gregory <shad@spinoza>
2020-07-23 19:02:28 -0500

    implement tag filter and add icon

--- src/hagen/core.clj
+++ src/hagen/core.clj
@@ -7,6 +7,7 @@
             [clojure.string :as str]
             [clojure.walk :refer [keywordize-keys]]
             [ring.util.codec :refer [form-decode]]
+            [ring.middleware.file :refer [wrap-file]]
             [ring.middleware.webjars :refer [wrap-webjars]]))
 
 (def db (atom
@@ -39,13 +40,15 @@
      (include-css "/assets/bootswatch-litera/bootstrap.min.css"))])
 
 (defn posts-page [& args]
-  (let [start (Integer. (:start (first args) 1))]
+  (let [start (Integer. (:start (first args) 1))
+        tag (:tag (first args))]
     (html5
      (posts-head)
      [:body
-      [:nav.navbar.navbar-expand-sm.navbar-dark.bg-primary
+      [:nav.navbar.navbar-expand-sm.navbar-light.bg-light
        [:div.container
-        [:a.navbar-brand {:href "/"} (:brand @config)]
+        [:a.navbar-brand {:href "/"}
+         [:img {:src "/img/chest.png" :style "padding-right:2px;"}] (:brand @config)]
         [:div#navbar_collapse.collapse.navbar-collapse
          [:ul.navbar-nav.mr-auto
           [:li.nav-item.dropdown
@@ -56,44 +59,51 @@
             (for [tag (sort (distinct (flatten (for [row @db] (:tags (val row))))))]
               [:a.dropdown-item {:href (str "/tags/" tag)} tag])]]
           [:li.nav-item
-           [:a.nav-link {:href "/rss"} "RSS"]]]]]]
+           [:a.nav-link {:target "_blank" :href "/rss"} "RSS"]]]]]]
       [:div.container
        [:div.row
         [:div.content.col-md-12
          [:h1 "Welcome"]
          [:p (:sub-brand @config)]
          [:div
-          (for [post (for [x (range (dec start) (if (< (+ start 4) (count @db))
-                                                  (+ start 4)
-                                                  (count @db)))]
-                       (nth (sort #(let [[m1 d1 y1] (str/split (:date (val %1)) #"/")
-                                         [m2 d2 y2] (str/split (:date (val %2)) #"/")]
-                                     (cond
-                                       (not (zero? (compare y2 y1))) (compare y2 y1)
-                                       (not (zero? (compare m2 m1))) (compare m2 m1)
-                                       (not (zero? (compare d2 d1))) (compare d2 d1)
-                                       :else 0)) @db) x))]
-            (if (or (nil? (:tag args)) (member? (first (:tag args)) (:tags (val post))))
-              [:article
-               [:a {:name (clojure.string/replace (key post) #" " "_")}]
-               [:headers
-                [:h2.text-info (key post)]
-                [:p.date-and-tags
-                 (:date (val post)) " :: "
-                 (for [tag (:tags (val post))]
-                   [:span {:style "padding-right: 4px;"}
-                    [:a.text-warning {:href (str "/tags/" tag)} tag]])]
-                [:p (:body (val post))]]]))
+          (let [the-db (filter #(or (nil? tag) (member? tag (:tags (val %))))
+                               @db)
+                curr-db (for [x (range (dec start) (if (< (+ start 4) (count the-db))
+                                                     (+ start 4)
+                                                     (count the-db)))]
+                          (nth (sort #(let [[m1 d1 y1] (str/split (:date (val %1)) #"/")
+                                            [m2 d2 y2] (str/split (:date (val %2)) #"/")]
+                                        (cond
+                                          (not (zero? (compare y2 y1))) (compare y2 y1)
+                                          (not (zero? (compare m2 m1))) (compare m2 m1)
+                                          (not (zero? (compare d2 d1))) (compare d2 d1)
+                                          :else 0)) the-db) x))]
+            (prn (count curr-db))
+            (prn (count the-db))
+            [:div
+             (for [post curr-db]
+               [:article
+                [:a {:name (clojure.string/replace (key post) #" " "_")}]
+                [:headers
+                 [:h2.text-info (key post)]
+                 [:p.date-and-tags
+                  (:date (val post)) " :: "
+                  (for [tag (:tags (val post))]
+                    [:span {:style "padding-right: 4px;"}
+                     [:a.text-warning {:href (str "/tags/" tag)} tag]])]
+                 [:p (:body (val post))]]])
+             (if (> (count the-db) 5)
+               [:nav {:aria-label "..."}
+                [:ul.pagination
+                 (for [x (range 1 (inc (int (Math/ceil (/ (count the-db) 5)))))]
+                   [:li
+                    (if (= x (int (Math/ceil (/ start 5))))
+                      {:class "page-item active"}
+                      {:class "page-item"})
+                    [:a.page-link {:href (str "/?start=" (inc (* 5 (dec x))))} x]])]])]
+            );; let
           ];;div
-         (if (> (count @db) 5)
-           [:nav {:aria-label "..."}
-            [:ul.pagination
-             (for [x (range 1 (inc (int (Math/ceil (/ (count @db) 5)))))]
-               [:li
-                (if (= x (int (Math/ceil (/ start 5))))
-                  {:class "page-item active"}
-                  {:class "page-item"})
-                [:a.page-link {:href (str "/?start=" (inc (* 5 (dec x))))} x]])]])]]]
+         ]]]
       (include-js "/assets/jquery/jquery.min.js")
       (include-js "https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/components/prism-clojure.js")
       (include-js "https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/components/prism-lisp.min.js")
@@ -101,11 +111,6 @@
 
 (defn handler [request]
   (cond
-    (not (nil? (re-find #"\/tags\/" (:uri request))))
-    {:status 200
-     :headers {"Content-Type" "text/html"}
-     :body (posts-page {:tag (let [tags (str/split (:uri request) #"\/")]
-                               (nth tags 2))})}
     (= "/rss" (:uri request)) {:status 200
                                :headers {"Content-Type" "text/xml"}
                                :body (str "<?xml version='1.0' encoding='UTF-8' ?>"
@@ -132,25 +137,38 @@
                                                                              (key (first posts))
                                                                              "</title>"
                                                                              "<description>"
-                                                                             (:body (val (first posts)))
+                                                                             (clojure.string/escape (:body (val (first posts)))
+                                                                                                    {\< "&lt;", \> "&gt;", \& "&amp;"})
                                                                              "</description>"
                                                                              "</item>"))))
                                           "</channel>"
                                           "</rss>")}
+    (re-find #"^\/tags" (:uri request)) (do
+                                          (prn "uri " (:uri request))
+                                          (prn "tag " (nth (clojure.string/split (:uri request) #"\/") 2))
+                                          {:status 200
+                                           :headers {"Content-Type" "text/html"}
+                                           :body (let [tag
+                                                       (nth (clojure.string/split (:uri request) #"\/") 2)]
+                                                   (posts-page {:tag tag}))})
     (= "/" (:uri request)) (let [args (if (not (nil? (:query-string request)))
                                         (keywordize-keys (form-decode (:query-string request))))]
                              {:status 200
                               :headers {"Content-Type" "text/html"}
                               :body (posts-page args)})
+    (not (nil? (re-find #"\/tags\/" (:uri request))))
+    {:status 200
+     :headers {"Content-Type" "text/html"}
+     :body (posts-page {:tag (let [tags (str/split (:uri request) #"\/")]
+                               (nth tags 2))})}
     :else {:status 404
            :body (html5 (posts-head)
                         [:body [:h2 "Page not found."]])}))
 
 (defn -main [& args]
-  (jetty/run-jetty (wrap-webjars handler) {:port (:port @config)}))
-
-(defn start []
-  (jetty/run-jetty (wrap-webjars handler) {:port (:port @config)}))
+  (jetty/run-jetty
+   (wrap-file(wrap-webjars handler) "public")
+   {:port (:port @config)}))
 
 (defn defpost
   ([title body]
