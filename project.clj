adb8c67e73b44ace916b39eb1eba99dac30991f2
Shad Gregory <shad@shadgregory.net>
2023-11-16 10:02:05 -0600

    update deps

--- project.clj
+++ project.clj
@@ -3,17 +3,17 @@
   :url "https://wsgregory.us"
   :license {:name "LGPL 3.0"
             :url "https://www.gnu.org/licenses/lgpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [hiccup "1.0.5"]
                  [ring-webjars "0.2.0"]
-                 [ring/ring "1.9.5"]
+                 [ring/ring "1.10.0"]
                  [garden "1.3.10"]
-                 [nrepl "0.9.0"]
-                 [org.slf4j/slf4j-simple "1.7.36"]
-                 [org.webjars.npm/bootstrap "5.1.3"]
-                 [org.webjars/jquery "3.6.0"]
-                 [org.webjars.npm/bootswatch "5.1.3"]
-                 [ring/ring-jetty-adapter "1.9.5"]]
+                 [nrepl "1.1.0"]
+                 [org.slf4j/slf4j-simple "2.0.9"]
+                 [org.webjars.npm/bootstrap "5.3.2"]
+                 [org.webjars/jquery "3.7.1"]
+                 [org.webjars.npm/bootswatch "5.3.2"]
+                 [ring/ring-jetty-adapter "1.10.0"]]
   :main ^:skip-aot hagen.posts
   :plugins [[lein-ring "0.12.5"]
             [hiccup-bridge "1.0.1"]
